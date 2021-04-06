140ae5b1a0d951749fc839bdc30fbf652da59971
ZackaryIngram <zingramingram@gmail.com>
2021-04-06 00:37:04 -0400

    Just started

---
+++ Adv Prog A2/Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	//Variables for game
+	string roomOneChoice, run, fight;
+	string roomTwoChoice;
+	string roomThreeChoice;
+	string roomFourChoice;
+
+	//Welcome player to game
+	cout << "Welcome to Zombie Simulater !" << endl;
+
+	//Read files conataining the storyline here
+	cout << "A science experiment gone wrong, has resulted in a new deadly virus infecting the living, and dead ! A zombie outbreak is bound to happen soon, can you survice the night of the dead ? Or will the undead feast on your brains ?" << endl;
+
+	//Start game 
+	cout << "Room 1" << endl;
+	cout << "One chilling evening, an emergency alert pops up on the TV. The Prime Minister of Canada says 'This is not a test, I repeat this is not a test, there has been a zombies outbreak!'. The TV cuts out, and there is the chilling sound of the undead swarming your house. They immedietely start breaking down the windows. " << endl;
+	cout << "Choices: ";
+	
+	
+
+	return 0;
+}
